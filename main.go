39b54c2d5ebff68b006b999784152a91b0c764b8
midorigreen <stitow14@gmail.com>
2017-11-09 22:05:53 +0900

    Add health check api

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/graphql-go/handler"
 	"github.com/midorigreen/gprof/prof"
 )
+
+const port = ":8080"
 
 func run() error {
 	h := handler.New(
@@ -17,7 +20,11 @@ func run() error {
 		})
 
 	http.Handle("/gq", h)
-	return http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/ping", func(w http.ResponseWriter, req *http.Request) {
+		fmt.Fprint(w, "OK")
+	})
+	log.Printf("server run %s port", port)
+	return http.ListenAndServe(port, nil)
 }
 
 func main() {
